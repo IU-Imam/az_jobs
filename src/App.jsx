5f446753660d670b2b4d1bfe3125cc0d0affaeaf
IU-Imam <imamuddin48@gmail.com>
2024-12-20 04:59:34 +0600

    Job listing done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 const App = () => {
   return (
   <>
   <Navbar/>
   <Hero/>
   <HomeCards/>
+  <JobListings/>
+  <ViewAllJobs/>
   </>
   )
 }
