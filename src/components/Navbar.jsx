ee896d803f431387153fd6bbe551c27215760f37
IU-Imam <imamuddin48@gmail.com>
2024-12-20 03:30:11 +0600

    Hero -created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../assets/images/logo.png'
 const Navbar = () => {
   return (
-    <nav className="bg-white-700 border-b border-indigo-500">
+    <nav className="bg-white-700 border-b border-zinc-400">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
           <div
