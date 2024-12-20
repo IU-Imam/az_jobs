ca07288cce76a99b0c4203e7ae04170302de05d3
IU-Imam <imamuddin48@gmail.com>
2024-12-20 21:30:29 +0600

    Conditional Rendering

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import JobListings from '../components/JobListings'
 
 const JobsPage = () => {
-  return (
-    <div>JobsPage</div>
-  )
+  return <section className="bg-teal-50 px-4 py-6" >
+    <JobListings/>
+  </section>
 }
 
 export default JobsPage
