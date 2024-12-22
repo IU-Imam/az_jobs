ef490f2737133b4df26d37bc68b09bcc74724c82
Mohammad Imam Uddin <69372901+IU-Imam@users.noreply.github.com>
2024-12-23 02:56:45 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
+  server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'https://676874d5cbf3d7cefd37fc13.mockapi.io/api',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/, ''),
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
-})
+});
